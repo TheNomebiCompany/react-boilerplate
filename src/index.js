3efca6fae85d1fc385aea583d794213b63b2e845
Cedric Guerin <guerin.ced@gmail.com>
2018-05-24 15:09:13 +0200

    Add react config

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import _ from 'lodash'
-import printMe from './print'
+import React from 'react'
+import { render } from 'react-dom'
 
-function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+const App = () => (
+    <div>
+        React App!
+    </div>
+)
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
-
-    return element;
-}
-
-document.body.appendChild(component());
+render(<App/>, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,27 @@ module.exports = {
     entry: './src/index.js',
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        port: 3001
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
-            title: 'Dc Brain'
+            template: path.join(__dirname, 'src/index.tpl.html'),
+            filename: 'index.html',
         })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader',
+                ],
+            },
+        ],
+    },
     output: {
         filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist'),
